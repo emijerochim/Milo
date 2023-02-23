b8f8a23ce02a2a9fafd7bac193d0ddf11be656a4
emijerochim <emijerochim@gmail.com>
2023-02-22 22:56:04 -0300

    refactor

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const { Client, Intents } = require("discord.js");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-dotenv = require("dotenv-flow").config();
-
-const config = {
-  token: process.env.DISCORD_TOKEN,
-  owner: process.env.OWNER_ID,
-  prefix: process.env.PREFIX,
-};
 
 client.once("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
@@ -16,4 +11,4 @@ client.on("message", (msg) => {
   console.log("Working!");
 });
 
-client.login(config.token);
+client.login(process.env.DISCORD_TOKEN);
